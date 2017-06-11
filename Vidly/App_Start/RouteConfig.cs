f62f31166994f536ac9482c43e975b4de92f8ec4
alhafi <Alhafi@YogaBook>
2017-06-12 01:46:06 +0300

    Attributes Route & Constraints

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,12 +12,14 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-                 "ReleasedDate",
-                "{Movies}/{ReleasedDate}/{year}/{month}",
-                new { controller = "Movies", action = "ReleasedDate" },
-                new { year = @"\d{4}", month = @"\d{2}" } /*we can put { month="\\d{ 2}" } || { year ={ "2015 | 2016"} }*/
-            );
+
+            routes.MapMvcAttributeRoutes();
+            //routes.MapRoute(
+            //     "ReleasedDate",
+            //    "{Movies}/{ReleasedDate}/{year}/{month}",
+            //    new { controller = "Movies", action = "ReleasedDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" } /*we can put { month="\\d{ 2}" } || { year ={ "2015 | 2016"} }*/
+            //);
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,19 +10,19 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index(int ? pageIndex,string sortBy)
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
             if (!pageIndex.HasValue)
                 pageIndex = 1;
             if (string.IsNullOrWhiteSpace(sortBy))
                 sortBy = "Name";
 
-            return Content(string.Format("PageIndex={0} & SortBy={1}",pageIndex,sortBy));
+            return Content(string.Format("PageIndex={0} & SortBy={1}", pageIndex, sortBy));
         }
         public ActionResult Random()
         {
             var movie = new Movie { Name = "Gladiator" };
-              return View(movie);
+            return View(movie);
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
@@ -30,14 +30,15 @@ namespace Vidly.Controllers
             // return RedirectToAction("Index","Home",new { page=1,sortby="Name"});
         }
         public ActionResult Edit(int Id)
-            //parameter sources
-            //movies/edit/1 embeded
-            //movie/edit?Id=1 passing parameter query string
-            //by entering in form Id=1
+        //parameter sources
+        //movies/edit/1 embeded
+        //movie/edit?Id=1 passing parameter query string
+        //by entering in form Id=1
         {
             return Content("ID=" + Id);
         }
 
+        [Route("Movies/ReleasedDate/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")] /*MIN,max.minlength,..etc google it attribute route constraints*/
         public ActionResult ReleasedDate(int ?year , byte month)
         {
             return Content("Year =" + year + "& Month = " + month);
