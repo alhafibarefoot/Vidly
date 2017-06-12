fa5f02cf9df4c19462406f9d0e23f64713f8b588
alhafi <Alhafi@YogaBook>
2017-06-13 00:24:57 +0300

    add-migration addAnnotation-CustomerName

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;  
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]   /*need to add using System.ComponentModel.DataAnnotations; */
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
