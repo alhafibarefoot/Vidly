79e55e7d263d1aafc3a48eae0ec29acb4bd30ce3
alhafi <Alhafi@YogaBook>
2017-06-12 19:38:19 +0300

    ViewModel Movie&Customers

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -22,10 +23,23 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie { Name = "Gladiator" };
-            ViewData["Movie"] = movie;
+            var customers = new List<Customer>
+            {
+                new Customer { Name="Customer1" } ,
+                new Customer { Name="Customer2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers=customers
+
+            };
+
+            return View(viewModel);
+            //ViewData["Movie"] = movie;
             //ViewBag.Movie= movie;
             //return View();
-           // return View(movie);
+            // return View(movie);
             //return Content("Hello World");
             //return HttpNotFound();
             //return new EmptyResult();
