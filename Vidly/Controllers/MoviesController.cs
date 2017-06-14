c22e06d18fe294af7a72b1a8a2ac7f30ce88648f
alhafi <Alhafi@YogaBook>
2017-06-15 02:11:40 +0300

    Excersise-MovieList

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
-using System.Data.Entity; /*Need for forign tables*/
+using System.Data.Entity;   /*This used in eager to include forign key*/
 
 namespace Vidly.Controllers
 {
@@ -22,26 +19,26 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
-
         // GET: Movies
-        public ActionResult Index1(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+        //public ActionResult Index1(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
 
-            return Content(string.Format("PageIndex={0} & SortBy={1}", pageIndex, sortBy));
+        //    return Content(string.Format("PageIndex={0} & SortBy={1}", pageIndex, sortBy));
 
 
-            
-        }
+
+        //}
 
         public ActionResult Index()
         {
             // var movies = GetMovies();
 
             var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
@@ -56,53 +53,53 @@ namespace Vidly.Controllers
 
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Gladiator" },
-                new Movie { Id = 2, Name = "Super Man" }
-            };
-        }
-        public ActionResult Random()
-        {
-            var movie = new Movie { Name = "Gladiator" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name="Customer1" } ,
-                new Customer { Name="Customer2" }
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers=customers
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie { Id = 1, Name = "Gladiator" },
+        //        new Movie { Id = 2, Name = "Super Man" }
+        //    };
+        //}
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie { Name = "Gladiator" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name="Customer1" } ,
+        //        new Customer { Name="Customer2" }
+        //    };
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers=customers
 
-            };
+        //    };
 
-            return View(viewModel);
-            //ViewData["Movie"] = movie;
-            //ViewBag.Movie= movie;
-            //return View();
-            // return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home");
-            // return RedirectToAction("Index","Home",new { page=1,sortby="Name"});
-        }
-        public ActionResult Edit(int Id)
-        //parameter sources
-        //movies/edit/1 embeded
-        //movie/edit?Id=1 passing parameter query string
-        //by entering in form Id=1
-        {
-            return Content("ID=" + Id);
-        }
+        //    return View(viewModel);
+        //    //ViewData["Movie"] = movie;
+        //    //ViewBag.Movie= movie;
+        //    //return View();
+        //    // return View(movie);
+        //    //return Content("Hello World");
+        //    //return HttpNotFound();
+        //    //return new EmptyResult();
+        //    //return RedirectToAction("Index", "Home");
+        //    // return RedirectToAction("Index","Home",new { page=1,sortby="Name"});
+        //}
+        //public ActionResult Edit(int Id)
+        ////parameter sources
+        ////movies/edit/1 embeded
+        ////movie/edit?Id=1 passing parameter query string
+        ////by entering in form Id=1
+        //{
+        //    return Content("ID=" + Id);
+        //}
 
-        [Route("Movies/ReleasedDate/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")] /*MIN,max.minlength,..etc google it attribute route constraints*/
-        public ActionResult ReleasedDate(int ?year , byte month)
-        {
-            return Content("Year =" + year + "& Month = " + month);
-        }
+        //[Route("Movies/ReleasedDate/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")] /*MIN,max.minlength,..etc google it attribute route constraints*/
+        //public ActionResult ReleasedDate(int ?year , byte month)
+        //{
+        //    return Content("Year =" + year + "& Month = " + month);
+        //}
     }
 }
