b556301bf0e0786c527626fb5bf324e5549a2e16
alhafi <Alhafi@YogaBook>
2017-06-19 00:09:32 +0300

    Exercise - Add validation to movie form

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
-using Vidly.Migrations;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -22,28 +21,13 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
-        // GET: Movies
-        public ActionResult Index1(int? pageIndex, string sortBy)
+
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(string.Format("PageIndex={0} & SortBy={1}", pageIndex, sortBy));
-
-
-
-        }
-
-        public ActionResult Index()
-        {
-            // var movies = GetMovies();
             var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
-
-
 
         public ViewResult New()
         {
@@ -64,18 +48,47 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
             return View("MovieForm", viewModel);
         }
 
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+             {
+                 new Customer { Name = "Customer 1" },
+                 new Customer { Name = "Customer 2" }
+             };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
@@ -94,68 +107,5 @@ namespace Vidly.Controllers
 
             return RedirectToAction("Index", "Movies");
         }
-
-        public ActionResult Details(int id)
-        {
-            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
-
-            if (movie == null)
-                return HttpNotFound();
-
-            return View(movie);
-
-        }
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Gladiator" },
-                new Movie { Id = 2, Name = "Super Man" }
-            };
-        }
-        public ActionResult Random()
-        {
-            var movie = new Movie { Name = "Gladiator" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name="Customer1" } ,
-                new Customer { Name="Customer2" }
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-
-            };
-
-            return View(viewModel);
-            //ViewData["Movie"] = movie;
-            //ViewBag.Movie= movie;
-            //return View();
-            // return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home");
-            // return RedirectToAction("Index","Home",new { page=1,sortby="Name"});
-        }
-        public ActionResult Edit1(int Id)
-        //parameter sources
-        //movies/edit/1 embeded
-        //movie/edit?Id=1 passing parameter query string
-        //by entering in form Id=1
-        {
-            return Content("ID=" + Id);
-        }
-
-        [Route("Movies/ReleasedDate/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")] /*MIN,max.minlength,..etc google it attribute route constraints*/
-        public ActionResult ReleasedDate(int? year, byte month)
-        {
-            return Content("Year =" + year + "& Month = " + month);
-        }
-
-
-
     }
 }
