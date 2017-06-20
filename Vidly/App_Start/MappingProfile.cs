2d841b77b9c6f832f21e29d43ef9efe3020219fe
alhafi <Alhafi@YogaBook>
2017-06-20 17:47:56 +0300

    Install Automapper 6.1 & define mapping profile

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            // Domain to Dto
+           CreateMap<Customer, CustomerDto>();  
+            // Dto to Domain
+           CreateMap<CustomerDto, Customer>();
+
+            
+
+        }
+    }
+}
