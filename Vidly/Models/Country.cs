1486935cc05129ffd763a86c6e5d70f46448eb06
alhafi@emisos.com <alhafi@emisos.com>
2017-06-30 08:25:30 +0300

    Add2TablesCountriesAndProvices

---
+++ Vidly/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;  
+
+namespace Vidly.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public virtual IList<ProvinceState> ProvinceStates { get; set; }
+    }
+}
