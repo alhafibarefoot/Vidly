d61bffe6f3720fb7cbb0f2f1686e5eb5079055d9
alhafi@emisos.com <alhafi@emisos.com>
2017-06-30 18:19:53 +0300

    Add JQuery.DataTable and combine referances with One referance LIB

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace Vidly
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/scripts/bootbox.js",//add bootbox reference which abstract over bootstrap
+                        "~/Scripts/respond.js",
+                        "~/scripts/datatables/jquery.datatables.js", //add jquery.datatables reference
+                        "~/scripts/datatables/datatables.bootstrap.js"  //integrate datatable with bootstrap
+                      ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,13 +25,11 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                       "~/scripts/bootbox.js", //add bootbox reference which abstract over bootstrap
-                      "~/Scripts/respond.js"));
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-Lumen.css",
+                      "~/content/datatables/css/datatables.bootstrap.css", //add table style sheet CSS bundle
                       "~/Content/site.css"));
         }
     }
