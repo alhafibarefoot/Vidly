d7938decd962429b55989b5668b11a4c38b70430
alhafi@emisos.com <alhafi@emisos.com>
2017-06-30 08:33:05 +0300

    SeedingDataFromCSV

--- Vidly/Migrations/Configuration.cs
+++ Vidly/Migrations/Configuration.cs
@@ -1,9 +1,14 @@
+using EntityFramework.Seeder;
 namespace Vidly.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+     using System.IO;
+    using System.Reflection;
+    using System.Text;
+    using Vidly.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Vidly.Models.ApplicationDbContext>
     {
@@ -13,19 +18,16 @@ namespace Vidly.Migrations
         }
 
         protected override void Seed(Vidly.Models.ApplicationDbContext context)
+       
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Countries.SeedFromResource("Vidly.Models.SeedData.countries.csv", c => c.Code);
+            context.SaveChanges();
+            context.ProvinceStates.SeedFromResource("Vidly.Models.SeedData.provincestates.csv", p => p.Code,
+                new CsvColumnMapping<ProvinceState>("CountryCode", (state, countryCode) =>
+                {
+                    state.Country = context.Countries.Single(c => c.Code == countryCode);
+                })
+             );            
         }
     }
 }
