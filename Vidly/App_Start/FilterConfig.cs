84e31840509e726feb048164b53f794379f61e9f
alhafi@emisos.com <alhafi@emisos.com>
2017-07-01 22:35:39 +0300

    Disable http Enterance

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace Vidly
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute()); //this will authorized all pages we have to add [AllowAnonymous] to home page at least 
+            filters.Add(new RequireHttpsAttribute()); //disable http: entrance
         }
     }
 }
