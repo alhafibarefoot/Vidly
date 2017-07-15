7d84b6ca9b1119c5b9a7f94c39a53384d503aa41
AlhafiYoga <AlhafiYoga@ALHAFIYOGA>
2017-07-15 19:13:24 +0300

    install-package toaster

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace Vidly
                         "~/Scripts/respond.js",
                         "~/scripts/datatables/jquery.datatables.js", //add jquery.datatables reference
                         "~/scripts/datatables/datatables.bootstrap.js" , //integrate datatable with bootstrap
-                        "~/scripts/typeahead.bundle.js" //see example google type ahead and add in content css typeahead
+                        "~/scripts/typeahead.bundle.js", //see example google type ahead and add in content css typeahead
+                         "~/scripts/toastr.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -32,6 +33,7 @@ namespace Vidly
                       "~/Content/bootstrap-Lumen.css",
                       "~/content/datatables/css/datatables.bootstrap.css", //add table style sheet CSS bundle
                        "~/content/typeahead.css",
+                        "~/content/toastr.css",
                       "~/Content/site.css"));
         }
     }
