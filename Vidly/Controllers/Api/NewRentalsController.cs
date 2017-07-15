912ccda38474054b41d121679e9f26c17958ff5d
AlhafiHPserver <alhafi@hotmail.com>
2017-07-15 10:40:17 +0300

    Definsive VS Optimistic:Definsive

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -24,16 +24,32 @@ namespace Vidly.Controllers.Api
             //one or more movie are Invalid
             //one or more movie Not available
 
+            //Definsive VS Optimistic
 
-            var customer = _context.Customers.Single(
-                c => c.Id == newRental.CustomerId);
+            //****************Definsive Approch
+            //helpfull when you use public api used by differant application and different teams
+            //for enternal use I prefer optimistic less condiftion less validation
 
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("no MovieID have been given"); //we move up because no movie so nesseccary quary customer
+
+            var customer = _context.Customers.SingleOrDefault(
+               c => c.Id == newRental.CustomerId);
+            if (customer == null)
+                return BadRequest("Invalid Cutomer ID");
+
+           
 
             var movies = _context.Movies.Where(
                 m => newRental.MovieIds.Contains(m.Id)).ToList(); //same select * from movie where id in (1,2,3);
 
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("one or more movieIds are Invalid");
+
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable==0)
+                    return BadRequest("one or more movie Not available");
 
                 movie.NumberAvailable--;
                 var rental = new Rental
@@ -45,6 +61,35 @@ namespace Vidly.Controllers.Api
 
                 _context.Rentals.Add(rental);
             }
+
+            //**************
+
+            //****************Optimistic Approch
+
+            //var customer = _context.Customers.Single(
+            //    c => c.Id == newRental.CustomerId);
+
+
+            //var movies = _context.Movies.Where(
+            //    m => newRental.MovieIds.Contains(m.Id)).ToList(); //same select * from movie where id in (1,2,3);
+
+            //foreach (var movie in movies)
+            //{
+
+            //    movie.NumberAvailable--;
+            //    var rental = new Rental
+            //    {
+            //        Customer = customer,
+            //        Movie = movie,
+            //        DateRented = DateTime.Now
+            //    };
+
+            //    _context.Rentals.Add(rental);
+            //}
+
+            //**************
+
+            
 
             _context.SaveChanges();
 
