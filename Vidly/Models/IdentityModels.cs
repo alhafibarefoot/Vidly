85ce70a90d0f12b685acf86a9cac7b0c5902ac5c
AlhafiHPserver <alhafi@hotmail.com>
2017-07-16 20:22:33 +0300

    Add custom error pages.

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Vidly.Models;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNet.Identity;
+
 
 namespace Vidly.Models
 {
