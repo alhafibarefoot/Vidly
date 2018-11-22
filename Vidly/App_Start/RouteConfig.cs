772adfe2e395795140d570161686926fc00a7d0e
A.Raheim Alhafi <alhafi@hotmail.com>
2018-11-22 10:11:10 +0300

    Add noted to routes

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Vidly.Controllers;
 
 namespace Vidly
 {
@@ -11,15 +12,28 @@ namespace Vidly
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");  //this ignore routering anything in debug /obj
 
-            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute("Home","",new { controller = "Movies", action = "Index" });  this if you need default http://localhost go to moveie controller and action index method
+            //routes.MapRoute("Home","movie",new { controller = "Movies", action = "Index" });  this if you need default http://localhost/movie  
+
+            // var namespacesMovie = new[] { typeof(MoviesController) };  //I used this var if i have same name of controller in different area of project
+            // routes.MapRoute("Home", "movie", new { controller = "Movies", action = "Index" },namespacesMovie ); this if you need default http://localhost/movie I used var namespacesMovie
+
             //routes.MapRoute(
             //     "ReleasedDate",
             //    "{Movies}/{ReleasedDate}/{year}/{month}",
             //    new { controller = "Movies", action = "ReleasedDate" },
             //    new { year = @"\d{4}", month = @"\d{2}" } /*we can put { month="\\d{ 2}" } || { year ={ "2015 | 2016"} }*/
             //);
+
+            routes.MapMvcAttributeRoutes();
+            
+
+            
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
